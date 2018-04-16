8304cc1debb6a36c1481c8b42f553f20866da57a
Magnolia K <magnolia.k@icloud.com>
2018-04-17 06:00:12 +0900

    パラメータを受け取るように修正
     - パラメータ(name)を受け取る様に修正
     - パラメータが無い場合(null)、"Wolrd"を設定
     - ルート("/")以外は、404を返すように修正

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -5,12 +5,26 @@ import javax.servlet._
 import javax.servlet.http._
 
 class HelloServlet extends HttpServlet {
-  override def doGet(req: HttpServletRequest, res: HttpServletResponse): Unit = {
-    res.setContentType("text/html")
-    
-    val out = res.getWriter
-    out.println("<!DOCTYPE html>")
-    out.println("<h1>Hello World!")
-    out.println("</body></html>")
+  override def service(req: HttpServletRequest, res: HttpServletResponse): Unit = {
+    if (req.getMethod == "GET" && req.getPathInfo == "/") {
+
+      res.setContentType("text/html")
+
+      val name = Option(req.getParameter("name")) match {
+        case None    => "Wolrd"
+        case Some(p) => p
+      }
+
+      val out = res.getWriter
+      out.println("<!DOCTYPE html>")
+      out.println("<html><body>")
+      out.println(s"<h1>Hello, $name")
+      out.println("</body></html>")
+
+    } else {
+
+      res.sendError(HttpServletResponse.SC_NOT_FOUND)
+
+    }
   }
 }
